5722805888f9ba15763682bf58fb6a31e0d75238
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-10-09 17:23:21 +0200

    :sparkles: feat(task-5): add task 5, docstrings

--- src/mongodb/tasks.py
+++ src/mongodb/tasks.py
@@ -9,8 +9,16 @@ class Task:
 
     @timed
     def task1(self):
+        """
+        How many users, activities and trackpoints are there in the dataset (after it is
+        inserted into the database).
+        """
+
+        # Count number of users
         users = self.db.users.count_documents({})
+        # Count number of activities
         activities = self.db.activities.count_documents({})
+        # Count number of track points
         track_points = self.db.track_points.count_documents({})
         return pd.DataFrame(
             {
@@ -23,14 +31,23 @@ class Task:
 
     @timed
     def task2(self):
+        """
+        Find the average number of activities per user.
+        """
+
         res = self.db.users.aggregate(
             [
+                # Count the number of activities per user
                 {"$project": {"activity_count": {"$size": "$activities"}}},
                 {
                     "$group": {
+                        # Group everything
                         "_id": {},
+                        # Find minimum activity count
                         "min_activity_count": {"$min": "$activity_count"},
+                        # Find maximum activity count
                         "max_activity_count": {"$max": "$activity_count"},
+                        # Find average activity count
                         "avg_activity_count": {"$avg": "$activity_count"},
                     }
                 },
@@ -40,10 +57,17 @@ class Task:
 
     @timed
     def task3(self):
+        """
+        Find the top 20 users with the highest number of activities.
+        """
+
         res = self.db.users.aggregate(
             [
+                # Count the number of activities for each user
                 {"$project": {"activity_count": {"$size": "$activities"}}},
+                # Sort by activitiy count, descending
                 {"$sort": {"activity_count": -1}},
+                # Limit to 20
                 {"$limit": 20},
             ]
         )
@@ -51,11 +75,42 @@ class Task:
 
     @timed
     def task4(self):
+        """
+        Find all users who have taken a taxi.
+        """
+
         res = self.db.activities.find(
+            # Find users that have taken a taxi
             {"transportation_mode": "taxi"},
+            # Only include user_id
             {"user_id": 1},
         ).distinct("user_id")
         return pd.DataFrame(list(res), columns=["Users taken taxi"])
+
+    @timed
+    def task5(self):
+        """
+        Find all types of transportation modes and count how many activities that are
+        tagged with these transportation mode labels. Do not count the rows where
+        the mode is null.
+        """
+
+        res = self.db.activities.aggregate(
+            [
+                # Remove entries without transportation mode, i.e. ""
+                {"$match": {"transportation_mode": {"$ne": ""}}},
+                # Group by transportation mode, using count aggregation
+                {
+                    "$group": {
+                        "_id": "$transportation_mode",
+                        "transportation_mode_count": {"$count": {}},
+                    }
+                },
+                # Sort by count per transportation mode, descending
+                {"$sort": {"transportation_mode_count": -1}},
+            ]
+        )
+        return pd.DataFrame(list(res))
 
 
 def main():
